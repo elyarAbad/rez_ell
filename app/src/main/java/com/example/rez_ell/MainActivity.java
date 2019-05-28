e34ecabd5a166695331a57fee77475f25d6eda76
elyarAbad <elyar.abad@gmail.com>
2019-05-28 22:47:39 +0430

    3rd

--- app/src/main/java/com/example/rez_ell/MainActivity.java
+++ app/src/main/java/com/example/rez_ell/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rez_ell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button but = findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                but.setText("clicked");
+            }
+        });
     }
 }
